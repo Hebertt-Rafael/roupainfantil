3109803499fe9c2481606849d48f120d72a99475
Hebertt Rafael <heberttrafael4528@gmail.com>
2026-03-03 07:36:50 -0300

    Delete roupainfantil directory

--- roupainfantil/src/main/java/com/example/roupainfantil/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.roupainfantil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.roupainfantil.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
